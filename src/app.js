fc157211a9ff0a5d9620ea0d74c04032f7e93019
Weiyi Lee <weiyilee@Weiyis-MacBook-Air.local>
2019-06-22 14:17:36 +0800

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,58 @@
 // entry -> output
 
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+
+module.exports = (env) => {
+
+	const isProduction = env === 'production';
+	const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+
+	return {
+		entry: ['babel-polyfill', './src/app.js'],
+		mode: 'development',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}]
+		},
+		plugins: [
+			CSSExtract
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		}
+	};
 };
+
 
 // history api fallback: handle rounting via client side code. It should return index.html for all 404 routes

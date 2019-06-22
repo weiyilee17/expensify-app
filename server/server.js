7146e9a71bdfa7fc93bdd6f4b49cb4c5df0563b7
Weiyi Lee <weiyilee@Weiyis-MacBook-Air.local>
2019-06-22 14:54:36 +0800

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+// use public directory to serve assets
+app.use(express.static(publicPath));
+
+// setting all unlisted routes to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('listening on port' + port);
+});

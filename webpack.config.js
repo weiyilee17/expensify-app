86bfc00cad5fd8492801be61c5f0c699fabd2bd9
Weiyi Lee <weiyilee@Weiyis-MacBook-Air.local>
2019-06-24 10:13:12 +0800

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
 		entry: ['babel-polyfill', './src/app.js'],
 		mode: 'development',
 		output: {
-			path: path.join(__dirname, 'public'),
+			path: path.join(__dirname, 'public', 'dist'),
 			filename: 'bundle.js'
 		},
 		module: {
@@ -49,7 +49,8 @@ module.exports = (env) => {
 		devtool: isProduction ? 'source-map' : 'inline-source-map',
 		devServer: {
 			contentBase: path.join(__dirname, 'public'),
-			historyApiFallback: true
+			historyApiFallback: true,
+			publicPath: '/dist/'
 		}
 	};
 };

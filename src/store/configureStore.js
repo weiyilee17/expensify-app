07dfe7faf287de44509b81c534ee7da4e35ef949
Weiyi Lee <weiyilee@Weiyis-MacBook-Air.local>
2019-07-10 17:20:58 +0800

    made redux dev tool extention work

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,7 +6,7 @@ import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Store creation
 
